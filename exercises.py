250c816d6130711588fdbb78042e71bf07b13ab3
fmorrow <fmorrow@uwo.ca>
2025-09-12 21:25:13 -0400

    first commit

---
+++ exercises.py
@@ -0,0 +1,22 @@
+execises = {
+    "chest": {
+        "bodyweight": ["Push-ups", "Chest Dips", "Chest Dips"],
+        "dumbbell": ["Dumbbell Bench Press", "Dumbbell Flyes", "Dumbbell Pullover", "Incline Dumbbell Press", "Single Arm Dumbbell Press"],
+        "barbell": ["Barbell Bench Press", "Incline Barbell Press", "Decline Barbell Press", "Close-Grip Bench Press", "EZ Barbell Pullover", "Wide Grip Dumbbell Bench Press"],
+        "machine": ["Chest Press Machine", "Pec Deck Machine", "Cable Crossover", "Cable Flyes", "Smith Machine Bench Press", "Incline Smith Machine Press", "Low Cable Crossover"]
+    },
+
+    "back": {
+        "bodyweight": ["Push-ups", "Chest Dips", "Chest Dips", "Pull Ups", "Chin Ups", "Inverted Rows", "Supermans", "Back Extensions", "Banded Rows", "Weighted Pull Ups", "Ring Rows"],
+        "dumbbell": ["Single Arm Bentover Dumbbell Rows", "Incline Dumbbell Rows", "Dumbbell Pullovers"],
+        "barbell": ["Deadlifts", "Banded Deadlifts", "Mid-shin Rack Deadlifts", "Mid-shin Banded Rack Deadlifts", "Bent Over Barbell Rows"],
+        "machine": ["Wide-grip Lat Pulldown", "Close-grip Lat Pulldown", "T-bar Rows", "Seated Cable Rows", "Single Arm Cable Rows", "Single Arm Lat Pulldown", "Back Extensions"]
+    },
+
+    "Legs": {
+        "Bodyweight": ["Bodyweight Calf Raises", "3-way Calf Raises", "Step-ups with Knee Raise", "Banded Leg Extensions", "Banded Leg Curls", "Air Squats", "Bulgarian Split Squats", "Lunges", "Jump Squats"],
+        "Dumbbell": ["Step Ups", "RDL's", "Lunges", "Goblet Squats", "Kettlebell Swings", "Bulgarian Split Squats", "Dumbbell Calf Raises"],
+        "Barbell": ["Standing Calf Raises", "Barbell Hip Thrusts", "Single Leg Hip Thrusts", "Deadlifts", "Back Squats", "Front Squats", "Box Squats", "Banded Box Squat", "Chain Box Squat", "Lunges", "Bulgarian Split Squats"],
+    }
+    
+}
