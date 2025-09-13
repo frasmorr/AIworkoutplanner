c6dc7892e28b52ba5b99c22141bd8aab542fc943
fmorrow <fmorrow@uwo.ca>
2025-09-12 22:01:58 -0400

    Updates exercises

--- exercises.py
+++ exercises.py
@@ -17,6 +17,27 @@ execises = {
         "Bodyweight": ["Bodyweight Calf Raises", "3-way Calf Raises", "Step-ups with Knee Raise", "Banded Leg Extensions", "Banded Leg Curls", "Air Squats", "Bulgarian Split Squats", "Lunges", "Jump Squats"],
         "Dumbbell": ["Step Ups", "RDL's", "Lunges", "Goblet Squats", "Kettlebell Swings", "Bulgarian Split Squats", "Dumbbell Calf Raises"],
         "Barbell": ["Standing Calf Raises", "Barbell Hip Thrusts", "Single Leg Hip Thrusts", "Deadlifts", "Back Squats", "Front Squats", "Box Squats", "Banded Box Squat", "Chain Box Squat", "Lunges", "Bulgarian Split Squats"],
-    }
+        "Machine": ["Thigh Abductor Machine", "Thigh Adductor Machine", "Smith Machine Calf Raises", "Seated Calf Raises", "Single Leg Horizontal Leg Raise", "Cable Kickbacks", "Step Ups with Knee Raise", "Single Leg Hip Thrust", "Quad Extensions", " Single Leg Press", "Leg Curls", "Hack Squats", "Leg Press"]
+    },
     
+    "Arms": {
+        "BodyWeight": ["TRX Bicep Curls", "TRX Tricep Extensions", "Diamond Push-ups", "Close-grip Push-ups", "Chin Ups", "Inverted Rows", "Tricep Dips", "Assisted Tricep Dips", "Bench Dips", "Banded Bicep Curls", "Banded Tricep Extensions", "Banded Overhead Tricep Extensions", "Banded Dart Throws"],
+        "Dumbbell": ["Hammer Curls", "Incline Hammer Curls", "Bicep Curls", "Incline Bicep Curls", "Spider Curls", "Curl 21's", "Zottman Curls", "Skull Crushers", "Overhead Tricep Extensions"],
+        "Barbell": ["Close-grip Bench Press", "Wide-grip Barbell Curls", "Close-grip Barbell Curls", "Preacher Curls", "EZ-bar Spider Curl", "EZ-bar Curls", "Barbell Curls", "Skull Crushers", "EZ-bar Skull Crushers", "Palms Up Wrist Curls", "Palms Down Wrist Curls", "Behind the Back Wrist Curls", "Reverse Grip Barbell Curls"],
+        "Machine": ["Machine Hammer Curls", "Machine Bicep Curls", "Machine Preacher Curls", "Back Cable Bicep Curls", "Tricep Pushdowns", "Tricep Kickbacks", "Tricep Rope Pushdowns", "Overhead Cable Tricep Extensions", "Single Arm Cable Tricep Extensions", "Dips Machine", "Cable Dart Throws"]
+    },
+
+    "Shoulders": {
+        "Bodyweight": ["Pike Push-ups", "Banded Facer Pulls", "Banded Lateral Raises", "Banded Front Raises", "Banded Shoulder Flyes"],
+        "Dumbbell": ["Dumbbell Shoulder Press", "Seated Dumbbell Shoulder Press", "Arnold Press", "Dumbbell Lateral Raises", "Dumbbell Front Raises", "Dumbbell Read Delt Flyes", "Dumbbell Shrugs", "YTW's", "Dumbbell Push Press", "Dumbbell Snatch", "Dumbbell Strict Press"],
+        "Barbell": ["Barbell Shrugs", "Barbell Push Press", "Barbell Overhead Strict Press", "Landmine Press", "Over and Back's"],
+        "Machine": ["Smith Machine Overhead Press", "Cable Lateral Raises", "Cable Front Raises", "Reverse Pec Deck Flyes"]
+    },
+
+    "Core": {
+        "Bodyweight": ["Plank", "Russian Twists", "Hanging Leg Raises", "Lying Knee Tucks", "Lying Leg Raises", "Bicycle Crunches", "Sit-ups", "V-ups", "Flutter Kicks", "Toe Touches", "Side Plank"],
+        "Dumbbell": ["Dumbbell Russian Twists", "Dumbbell Sit-ups", "Dumbbell V-ups"],
+        "Barbell": ["Landmine Twists", "Barbell Rollouts"],
+        "Machine": ["Machine Ab Crunch"],
+    }
 }
